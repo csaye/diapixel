3f4393ad531f0a4d7aeec37c0cccd7a66b44a19c
Cooper Saye <coopersaye@gmail.com>
2020-08-17 13:53:20 -0700

    Implement 2D camera movement.

---
+++ Assets/Diapixel/Sketch2D/CameraMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Diapixel.Sketch2D
+{
+    public class CameraMovement : MonoBehaviour
+    {
+        [Header("Attributes")]
+        [SerializeField] private float speed = 0;
+
+        private void Update()
+        {
+            Move();
+        }
+
+        private void Move()
+        {
+            Vector2 direction;
+
+            direction.x = Input.GetAxis("Horizontal");
+            direction.y = Input.GetAxis("Vertical");
+
+            transform.position = transform.position + (Vector3)(direction * speed * Time.deltaTime);
+        }
+    }
+}
