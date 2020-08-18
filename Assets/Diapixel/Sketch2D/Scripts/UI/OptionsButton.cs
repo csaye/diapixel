cf3a009a48cb2d86f301a3351ff86320c1fe7ec0
Cooper Saye <coopersaye@gmail.com>
2020-08-17 18:27:57 -0700

    Implement options panel.

---
+++ Assets/Diapixel/Sketch2D/Scripts/UI/OptionsButton.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Diapixel.Sketch2D.UI
+{
+    [RequireComponent(typeof(Animator))]
+    public class OptionsButton : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private GameObject optionsPanel = null;
+
+        private Animator animator;
+
+        private void Start()
+        {
+            animator = GetComponent<Animator>();
+        }
+        
+        public void TriggerPanel()
+        {
+            bool optionsPanelActive = optionsPanel.activeSelf;
+            optionsPanel.SetActive(!optionsPanelActive);
+
+            if (optionsPanelActive)
+            {
+                animator.SetTrigger("Close");
+            }
+            else
+            {
+                animator.SetTrigger("Open");
+            }
+        }
+    }
+}
