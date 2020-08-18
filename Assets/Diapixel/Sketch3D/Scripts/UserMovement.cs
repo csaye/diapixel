3fe42d9915da3af95d069c8f37d87d5b98ec681e
Cooper Saye <coopersaye@gmail.com>
2020-08-18 09:53:49 -0700

    Implement user movement.

---
+++ Assets/Diapixel/Sketch3D/Scripts/UserMovement.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+namespace Diapixel.Sketch3D
+{
+    [RequireComponent(typeof(CharacterController))]
+    public class UserMovement : MonoBehaviour
+    {
+        [Header("Attributes")]
+        [SerializeField] private float moveSpeed = 0;
+        [SerializeField] private float sprintSpeed = 0;
+        [SerializeField] private float flySpeed = 0;
+
+        private CharacterController controller;
+
+        private Vector3 flyVelocity;
+
+        private void Start()
+        {
+            controller = GetComponent<CharacterController>();
+
+            flyVelocity = new Vector3(0, flySpeed, 0);
+        }        
+
+        private void Update()
+        {
+            Move();
+            Fly();
+        }
+
+        private void Move()
+        {
+            if (Input.GetKey(KeyCode.LeftControl))
+            {
+                controller.Move(MoveDirection() * sprintSpeed * Time.deltaTime);
+            }
+            else
+            {
+                controller.Move(MoveDirection() * moveSpeed * Time.deltaTime);
+            }
+        }
+
+        private void Fly()
+        {
+            if (Input.GetButton("Jump"))
+            {
+                controller.Move(flyVelocity * Time.deltaTime);
+            }
+
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                controller.Move(-flyVelocity * Time.deltaTime);
+            }
+        }
+
+        private Vector3 MoveDirection()
+        {
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
+
+            return transform.right * horizontal + transform.forward * vertical;
+        }
+    }
+}
